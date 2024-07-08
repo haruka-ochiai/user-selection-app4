000b81e78cf080f9d6f3dfc9837e6b14be601d7e
haruka-ochiai <pw.g1.po1122@gmail.com>
2024-07-08 21:41:56 +0900

    first commit

---
+++ src/app/components/Gacha.js
@@ -0,0 +1,8 @@
+'use client'
+
+import { useState } from 'react'
+import data from '../data/users'
+
+const Gacha = () => {
+    
+}
---
+++ src/app/data/users.js
@@ -0,0 +1,19 @@
+const data = {
+    東京: {
+        departments: ["営業", "商品開発", "経理"],
+        users: {
+            "営業": ["User1", "User2", "User3"],
+            "商品開発": ["User4", "User5", "User6"],
+            "経理": ["User7", "User8", "User9"]
+        }
+    },
+    大阪: {
+        departments: ["総務", "マーケティング"],
+        users: {
+            "総務": ["User10", "User11", "User12"],
+            "マーケティング": ["User13", "User14", "User15"]
+        }
+    }
+};
+
+export default data;
